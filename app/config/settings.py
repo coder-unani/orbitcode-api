707af36ea90206c2ed388f0ac6fc1826c54cba60
UNANI <coder.unani@gmail.com>
2024-05-11 04:36:06 +0900

    [feat] CORS middleware 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 ENV_NAME = "development"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print("path")
-print(os.path.join(BASE_DIR, f".env.{ENV_NAME}"))
 load_dotenv(os.path.join(BASE_DIR, f".env.{ENV_NAME}"))
 
 class Settings:
@@ -20,6 +18,19 @@ class Settings:
     DB_USER_NAME: str = os.getenv('DB_USER_NAME')
     DB_USER_PASSWORD: str = os.getenv('DB_USER_PASSWORD')
 
+    # AWS S3
+    AWS_BUCKET_REGION: str = os.getenv('AWS_BUCKET_REGION')
+    AWS_BUCKET_NAME: str = os.getenv('AWS_BUCKET_NAME')
+    AWS_ACCESS_KEY_ID: str = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY: str = os.getenv('AWS_SECRET_ACCESS_KEY')
+    
+    # CORS origins
+    CORS_ORIGINS: list = [
+        "http://localhost",
+        "http://localhost:3000",
+        "http://localhost:8000",
+    ]
+    
 settings = Settings()
 
     
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes import contents
+from app.config.settings import settings
 
 def create_app():
 
@@ -9,6 +12,13 @@ def create_app():
     app = FastAPI()
 
     # Middleware 정의
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=settings.CORS_ORIGINS,
+        allow_credentials=True, # cross-origin request에서 cookie를 허용
+        allow_methods=["*"], # 모든 HTTP 메서드 허용
+        allow_headers=["*"], # 모든 HTTP 헤더 허용
+    )
 
     # Router 정의
     app.include_router(contents.router, prefix="/contents")
