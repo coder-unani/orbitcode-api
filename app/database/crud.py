575046668e7f61e42ca955e194e4146bc3547123
UNANI <coder.unani@gmail.com>
2024-05-11 04:37:08 +0900

    [feat] videos api 검색 조건 추가

--- app/database/crud.py
+++ app/database/crud.py
@@ -1,35 +1,29 @@
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
-from app.database import schemas, models
+from app.database.models import ContentsVideo
 
-def get_videos(db: Session, is_delete: bool|None = None):
-    videos = []
-    if is_delete is None:
-        videos = db.query(models.ContentsVideo).all()
+def get_videos(db: Session, page: int, is_delete: bool|None = None, is_confirm: bool|None = None, keyword: str|None = None):
+    unit_per_page = 20
+    offset = (page - 1) * unit_per_page
+
+    videos = db.query(ContentsVideo)
+    if is_delete is not None:
+        videos = videos.filter(ContentsVideo.is_delete == is_delete)
+    if is_confirm is not None:
+        videos = videos.filter(ContentsVideo.is_confirm == is_confirm)
+    if keyword is not None:
+        videos = videos.filter(ContentsVideo.title.contains(keyword, autoescape=True))
     else:
-        videos = db.query(models.ContentsVideo).filter(
-            models.ContentsVideo.is_delete == is_delete,
-            models.ContentsVideoWatch.is_delete == is_delete,
-            models.ContentsVideoGenre.is_delete == is_delete,
-            models.ContentsVideoStaff.is_delete == is_delete,
-            models.ContentsVideoAttach.is_delete == is_delete
-        ).all()
+        videos = db.query(ContentsVideo)
+
+    total = videos.count()
+    videos = videos.offset(offset).limit(unit_per_page).all()
+
+    return total, videos
+
+def get_video_by_id(db: Session, video_id: int):
+
+    video = db.query(ContentsVideo).filter(ContentsVideo.id == video_id).first()
     
-    return videos
-
-def get_video_by_id(db: Session, video_id: int, is_delete: bool|None = None):
-
-    if is_delete is None:
-        video = db.query(models.ContentsVideo).filter(models.ContentsVideo.id == video_id).first()
-    else:
-        video = db.query(models.ContentsVideo).filter(
-            models.ContentsVideo.id == video_id, 
-            models.ContentsVideo.is_delete == is_delete,
-            models.ContentsVideoWatch.is_delete == is_delete,
-            models.ContentsVideoGenre.is_delete == is_delete,
-            models.ContentsVideoStaff.is_delete == is_delete,
-            models.ContentsVideoAttach.is_delete == is_delete
-        ).first()
-        
     return video
