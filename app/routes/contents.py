905fd94fb09845e2c0baf26f7bfccbb618ec69cc
UNANI <coder.unani@gmail.com>
2024-05-23 18:36:10 +0900

    [feat] API 추가 구현 body: GET: /contents/videos GET: /contents/videos/{video_id} POST: /contents/videos/{video_id} PUT: /contents/videos/{video_id} DELETE: /contents/videos/{video_id}

--- app/routes/contents.py
+++ app/routes/contents.py
@@ -1,22 +1,24 @@
-import os
-from typing import Union
 from fastapi import APIRouter, Depends
+from fastapi.encoders import jsonable_encoder
 
 from sqlalchemy.orm import Session
 
 from app.database.database import get_db
-from app.database import schemas, crud
+from app.database import queryset
+from app.database.schemas import Response, ResponseVideo, ResponseVideos, ReuqestVideo
 from app.utils.s3client import S3Client
 
 router = APIRouter()
 
-@router.get("/videos", response_model=schemas.ResContentsVideos)
-async def contents_videos(
-    page: int = 1, 
-    is_delete: bool|None = None, 
-    is_confirm: bool|None = None,
-    keyword: str|None = None,
-    db: Session = Depends(get_db)
+
+# 비디오 목록 조회
+@router.get("/videos", response_model=ResponseVideos)
+async def content_videos(
+        page: int = 1,
+        is_delete: bool | None = None,
+        is_confirm: bool | None = None,
+        keyword: str | None = None,
+        db: Session = Depends(get_db)
 ):
     # page validation
     if page < 0:
@@ -26,31 +28,47 @@ async def contents_videos(
         return {"status": "fail", "message": "Keyword must be at least 2 characters"}
 
     # get videos
-    total, videos = crud.get_videos(db, page=page, is_delete=is_delete, is_confirm=is_confirm, keyword=keyword)
-    if not videos:
-        return {"status": "fail", "message": "Videos not found", "total": total, "page": page}
+    status, message, total, videos = queryset.read_videos(db, page, is_delete, is_confirm, keyword)
 
     # return videos
-    return {"total": total, "page": page, "data": videos}
+    return {"status": status, "message": message, "data": {"total": total, "page": page, "list": videos}}
 
-@router.get("/videos/{video_id}", response_model=schemas.ResContentsVideo)
-async def contents_videos_by_id(video_id: int, db: Session = Depends(get_db)):
-    video = crud.get_video_by_id(db, video_id=video_id)
+
+# 비디오 생성
+@router.post("/videos", response_model=Response)
+async def create_video(video: ReuqestVideo, db: Session = Depends(get_db)):
+    status, message = queryset.create_video(db, video=jsonable_encoder(video))
+    return {"status": status, "message": message}
+
+
+# 비디오 조회
+@router.get("/videos/{video_id}", response_model=ResponseVideo)
+async def read_video(video_id: int, db: Session = Depends(get_db)):
+    video = queryset.read_video(db, video_id=video_id)
 
     s3client = S3Client()
-    attachs = video.attachs
-    for i in range(len(attachs)):
-        attachs[i].attach_url = s3client.create_presigned_url(attachs[i].attach_url)
-    
-    video.attachs = attachs
+    thumbnail = video.thumbnail
+    for i in range(len(thumbnail)):
+        thumbnail[i].url = s3client.create_presigned_url(thumbnail[i].url)
+
+    video.thumbnail = thumbnail
     s3client.close()
 
     if not video:
         return {"status": "fail", "message": "Video not found"}
-    
+
     return {"data": video}
 
 
-@router.put("/videos/{video_id}", response_model=schemas.ResContentsVideo)
-async def contents_videos_update(video_id: int, video: schemas.ReqContentsVideo, db: Session = Depends(get_db)):
-    pass
+# 비디오 업데이트
+@router.put("/videos/{video_id}", response_model=Response)
+async def update_video(video_id: int, video: ReuqestVideo, db: Session = Depends(get_db)):
+    status, message = queryset.update_video(db, video_id=video_id, video=video)
+    return {"status": status, "message": message}
+
+
+# 비디오 삭제
+@router.delete("/videos/{video_id}", response_model=Response)
+async def delete_video(video_id: int, db: Session = Depends(get_db)):
+    status, message = queryset.delete_video(db, video_id=video_id)
+    return {"status": status, "message": message}
