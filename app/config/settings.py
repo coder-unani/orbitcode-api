08621f699c479d8263773aa0057eb80e4b59e790
UNANI <coder.unani@gmail.com>
2024-05-25 23:14:49 +0900

    [feat] APP 기본 설정값 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+from zoneinfo import ZoneInfo
+
 
 ENV_NAME = "development"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -8,8 +10,29 @@ load_dotenv(os.path.join(BASE_DIR, f".env.{ENV_NAME}"))
 
 class Settings:
     # Default
-    APP_NAME: str = "ORBITCODE APIs"
+    APP_NAME: str = os.getenv('APP_NAME')
     BASE_DIR: str = BASE_DIR
+    TIME_ZONE = ZoneInfo(os.getenv('TIME_ZONE'))
+
+    # Security
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    ALGORITHM: str = os.getenv('ALGORITHM')
+    ACCESS_TOKEN_EXPIRE_TIME: int = os.getenv('ACCESS_TOKEN_EXPIRE_TIME')
+    REFRESH_TOKEN_EXPIRE_TIME: int = os.getenv('REFRESH_TOKEN_EXPIRE_TIME')
+
+    # User
+    # 10: email, 11: google, 12: facebook, 13: kakao, 14: naver, 15: apple
+    USER_TYPE_ALLOW: list = ["10", "11", "12", "13", "14", "15"]
+    USER_EMAIL_ALLOW_SPACE: bool = False
+    USER_EMAIL_LENGTH_MIN: int = 5
+    USER_EMAIL_LENGTH_MAX: int = 50
+    USER_PASSWORD_INCLUDE_SPACE: bool = False
+    USER_PASSWORD_INCLUDE_WORD: bool = True
+    USER_PASSWORD_INCLUDE_NUMBER: bool = True
+    USER_PASSWORD_INCLUDE_SIMBOL: bool = True
+    USER_PASSWORD_INCLUDE_UPPER: bool = False
+    USER_PASSWORD_LENGTH_MIN: int = 8
+    USER_PASSWORD_LENGTH_MAX: int = 22
 
     # Database
     DB_DRIVER: str = os.getenv('DB_DRIVER')
