0a9f6fd763a1351d3bf33bfbfe5a17cea248292d
UNANI <coder.unani@gmail.com>
2024-05-25 23:12:09 +0900

    [feat] FastAPI 기본 탑재된 Swagger 커스터마이징 테스트

--- app/main.py
+++ app/main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routes import contents
+from app.routes import users, contents
 from app.config.settings import settings
+
+SWAGGER_HEADERS = {
+    "title": settings.APP_NAME,
+    "version": "0.1.0",
+    "description": "라라라라라라"
+}
 
 
 # FastAPI initialize
 def create_api() -> FastAPI:
-    api = FastAPI()
+    api = FastAPI(
+        swagger_ui_parameters={
+            "deepLinking": "true",
+            "displayRequestDuration": "true",
+            "docExpansion": "none",
+            "operationsSorter": "alpha",
+            "filter": True,
+            "tagsSorter": "alpha",
+            "syntaxHighlight.theme": "tomorrow-night",
+        },
+        **SWAGGER_HEADERS
+    )
 
     # Middleware 정의
     api.add_middleware(
@@ -18,7 +35,10 @@ def create_api() -> FastAPI:
         allow_headers=["*"],  # 모든 HTTP 헤더 허용
     )
 
+    router = APIRouter()
+
     # Router 정의
+    api.include_router(users.router, prefix="/users")
     api.include_router(contents.router, prefix="/contents")
 
     return api
