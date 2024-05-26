b45c44af308de782955f1d079d5547aae7ada183
UNANI <coder.unani@gmail.com>
2024-05-26 21:58:45 +0900

    [fix] S3Client 로직 수정

--- app/utils/s3client.py
+++ app/utils/s3client.py
@@ -1,49 +1,70 @@
 import boto3
-import logging
 from botocore.exceptions import ClientError
 from app.config.settings import settings
 
 
 class S3Client:
-    def __init__(self, region=None):
-        self._aws_access_key_id = settings.AWS_ACCESS_KEY_ID
-        self._aws_secret_access_key = settings.AWS_SECRET_ACCESS_KEY
-        self._aws_bucket_name = settings.AWS_BUCKET_NAME
-        self._aws_bucket_region = settings.AWS_BUCKET_REGION
+    def __init__(
+            self,
+            aws_s3_access_key_id: str = settings.AWS_S3_ACCESS_KEY_ID,
+            aws_s3_secret_access_key: str = settings.AWS_S3_SECRET_ACCESS_KEY,
+            aws_s3_bucket_name: str = settings.AWS_S3_BUCKET_NAME,
+            aws_s3_bucket_region: str = settings.AWS_S3_BUCKET_REGION
+    ):
+        self.aws_s3_access_key_id = aws_s3_access_key_id
+        self.aws_s3_secret_access_key = aws_s3_secret_access_key
+        self.aws_s3_bucket_name = aws_s3_bucket_name
+        self.aws_s3_bucket_region = aws_s3_bucket_region
 
-        self._client = boto3.client(
+        self.client = boto3.client(
             "s3",
-            aws_access_key_id=self._aws_access_key_id,
-            aws_secret_access_key=self._aws_secret_access_key,
-            region_name=self._aws_bucket_region
+            aws_access_key_id=self.aws_s3_access_key_id,
+            aws_secret_access_key=self.aws_s3_secret_access_key,
+            region_name=self.aws_s3_bucket_region
         )
         
-    def upload_file(self, file_path, destination="attach/"):
-        file_name = file_path.split('/')[-1]  
-        destination = destination + file_name
-        print(f"Uploading {file_path} to {self._aws_bucket_name}/{destination}")
+    def upload_file(self, path_from, path_to):
         try:
-            self._client.upload_file(file_path, self._aws_bucket_name, destination)
-            print(f"File uploaded to {self._aws_bucket_name}/{destination}")
-            return destination
+            if not self.client:
+                raise ClientError("S3 client not initialized")
+            if not path_from or not path_to:
+                raise ClientError("Invalid file path or destination")
+            self.client.upload_file(path_from, self.aws_s3_bucket_name, path_to)
+            return path_to
         
         except ClientError as e:
-            logging.error(e)
+            str(e)
             return False
-    
+
+    def delete_file(self, object_name):
+        try:
+            if not self.client:
+                raise Exception("S3 client not initialized")
+            if not object_name:
+                raise Exception("Invalid object name")
+            result = self.client.delete_object(Bucket=self.aws_s3_bucket_name, Key=object_name)
+            return result
+        except Exception as e:
+            str(e)
+            return False
+
     def create_presigned_url(self, object_name, expiration=3600):
         try:
-            response = self._client.generate_presigned_url('get_object',
+            response = self.client.generate_presigned_url(
+                'get_object',
                 Params={
-                    'Bucket': self._aws_bucket_name,
-                    'Key': object_name},
-                    ExpiresIn=expiration
-                )
-        except ClientError as e:
-            logging.error(e)
+                    'Bucket': self.aws_s3_bucket_name,
+                    'Key': object_name
+                },
+                ExpiresIn=expiration
+            )
+            return response
+
+        except Exception as e:
+            print(e)
             return None
-        return response
 
     def close(self):
-        self._client.close()
+        if self.client:
+            self.client.close()
         
