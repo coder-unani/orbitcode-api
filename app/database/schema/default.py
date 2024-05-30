e111e4d3be5476f39b341e9dc57544389e66637a
UNANI <coder.unani@gmail.com>
2024-05-30 22:32:37 +0900

    [feat] 기본 Response Model 추가

--- app/database/schema/default.py
+++ app/database/schema/default.py
@@ -3,23 +3,16 @@ from datetime import datetime
 from pydantic import BaseModel
 
 
-class AccessLog(BaseModel):
-    id: int
-    status: int
-    path: str
-    ip: str
-    user_id: int
-    message: str
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
-
-
 class ResponseModel(BaseModel):
     status: str = "success"
     code: str = ""
     message: str = ""
+
+
+class ResponseDataModel(ResponseModel):
+    data: dict | None = None
+
+
 
 
 
