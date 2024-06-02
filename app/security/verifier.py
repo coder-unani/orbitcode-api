f74ec3f75b3b8284b97b3b0094fb3808b22aa245
UNANI <coder.unani@gmail.com>
2024-06-02 18:06:50 +0900

    [fix] 반복되는 코드 정리

--- app/security/verifier.py
+++ app/security/verifier.py
@@ -19,60 +19,39 @@ security = HTTPBasic()
 
 def verify_user(user: dict):
     if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "USER_LOGIN_AUTH_FAIL")
     return True
 
 
 def verify_user_email(email: str):
     if not email:
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "USER_LOGIN_AUTH_FAIL")
     return True
 
 
 def verify_user_password(password: str, hashed_password: str):
     if not password or not hashed_password:
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "USER_LOGIN_AUTH_FAIL")
     if not Password.verify_password(password, hashed_password):
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "USER_LOGIN_AUTH_FAIL")
     return True
 
 
 def verify_user_active(user: User):
     if not verify_user(user) or not user.is_active:
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "USER_LOGIN_AUTH_FAIL")
     return True
 
 
 def verify_user_unblocked(user: User):
     if not verify_user(user) or user.is_block:
-        raise HTTPException(
-            status_code=401,
-            detail="Blocked account"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "USER_BLOCKED")
     return True
 
 
 def verify_user_isadmin(user: User):
     if not verify_user(user) or not user.is_admin:
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_401_UNAUTHORIZED, "NO_PERMISSION")
     return True
 
 
@@ -82,33 +61,21 @@ def verify_access_token(request: Request):
         # 토큰 존재 유무 검증
         is_token, code, token = JWTManager.get_access_token(request)
         if not is_token or not token:
-            raise HTTPException(
-                status_code=401,
-                detail="Token is required"
-            )
+            return json_response(status.HTTP_400_BAD_REQUEST, "ACCESS_TOKEN_REQUIRE")
         # 토큰 정합성 검증
         decoded_token = JWTManager.decode_access_token(token)
         if not JWTManager.decode_access_token(token):
-            raise HTTPException(
-                status_code=401,
-                detail="Token is invalid"
-            )
+            return json_response(status.HTTP_400_BAD_REQUEST, "ACCESS_TOKEN_INVALID")
         # 토큰 만료 검증
         is_unexpire = JWTManager.verify_access_token_expire(decoded_token)
         if not is_unexpire:
-            raise HTTPException(
-                status_code=401,
-                detail="Token is expired"
-            )
+            return json_response(status.HTTP_400_BAD_REQUEST, "ACCESS_TOKEN_EXPIRED")
         # 결과 출력
         return token
     except HTTPException as e:
         raise e
     except Exception:
-        raise HTTPException(
-            status_code=401,
-            detail="Authentication failed"
-        )
+        return json_response(status.HTTP_500_INTERNAL_SERVER_ERROR, "EXCEPTION")
 
 
 def verify_access_token_user(request: Request, db: Session = Depends(get_db)):
