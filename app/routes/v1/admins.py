7f0b33f0f62b0bdfc383ec1b86c5321831b9e066
UNANI <coder.unani@gmail.com>
2024-06-06 00:31:44 +0900

    [feat] 관리자 상품 상세 조회 API 제작

--- app/routes/v1/admins.py
+++ app/routes/v1/admins.py
@@ -9,6 +9,7 @@ from app.database.queryset import videos as queryset
 from app.database.schema.default import Res
 from app.database.schema.videos import (
     ReqVideo,
+    ResVideoAdmin,
     ResVideosAdmin,
 )
 
@@ -93,6 +94,17 @@ async def admin_content_videos(
         return json_response(status.HTTP_200_OK, "VIDEO_NOT_FOUND")
     # 성공 응답 반환
     return {"message": "", "total": total, "count": count, "page": p, "data": videos}
+
+
+@router.get("/videos/{video_id}", tags=['contents'], response_model=ResVideoAdmin)
+async def read_video(video_id: int, db: Session = Depends(get_db)):
+    result, code, video = queryset.read_video_by_id(db, video_id=video_id)
+    if not result:
+        return json_response(status.HTTP_500_BAD_REQUEST, code)
+    if not video:
+        return json_response(status.HTTP_400_BAD_REQUEST, code)
+
+    return ResVideoAdmin(code=code, data=video)
 
 
 # 비디오 업데이트
