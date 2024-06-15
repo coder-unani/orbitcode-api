87e5f95792b3c1bf8d04a96821fb5b27acc93c3b
UNANI <coder.unani@gmail.com>
2024-06-15 22:07:04 +0900

    [feat] 이메일 마스킹 기능 제작

--- app/utils/formatter.py
+++ app/utils/formatter.py
@@ -1,3 +1,22 @@
+def format_mask_email(email):
+    """
+    이메일 주소를 마스킹합니다.
+
+    :param email: 마스킹할 이메일 주소
+    :return: 마스킹된 이메일 주소
+    """
+    if '@' not in email:
+        raise ValueError("Invalid email address")
+
+    local_part, domain_part = email.split('@')
+    if len(local_part) <= 2:
+        masked_local = local_part[0] + "*" * (len(local_part) - 1)
+    else:
+        masked_local = local_part[0] + "*" * (len(local_part) - 2) + local_part[-1]
+
+    masked_email = masked_local + "@" + domain_part
+    return masked_email
+
 def format_datetime(dt, fmt='%Y-%m-%d %H:%M:%S'):
     try:
         return dt.strftime(fmt)
