2b6b5e2423aadf444c209ea96148778b62181f41
UNANI <coder.unani@gmail.com>
2024-06-17 01:59:07 +0900

    [fix] log filename 변경

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -6,7 +6,7 @@ from logging.handlers import TimedRotatingFileHandler
 
 class Logger:
     def __init__(self, log_dir="logs", log_level=logging.INFO):
-        self.logger = logging.getLogger('FastAPI_Logger')
+        self.logger = logging.getLogger("FastAPI_Logger")
         self.logger.setLevel(log_level)
 
         # Ensure log directory exists
@@ -15,23 +15,27 @@ class Logger:
 
         # Info log handler
         info_handler = TimedRotatingFileHandler(
-            filename=os.path.join(log_dir, f"info.{datetime.now().year}{datetime.now().month}.log"),
-            when='midnight',
+            filename=os.path.join(log_dir, f"info.{datetime.today().date()}.log"),
+            when="midnight",
             interval=1,
-            backupCount=30
+            backupCount=30,
         )
         info_handler.setLevel(logging.INFO)
-        info_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        info_handler.setFormatter(
+            logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        )
 
         # Error log handler
         error_handler = TimedRotatingFileHandler(
-            filename=os.path.join(log_dir, f"error.{datetime.now().year}{datetime.now().month}.log"),
-            when='midnight',
+            filename=os.path.join(log_dir, f"error.{datetime.today().date()}.log"),
+            when="midnight",
             interval=1,
-            backupCount=30
+            backupCount=30,
         )
         error_handler.setLevel(logging.ERROR)
-        error_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        error_handler.setFormatter(
+            logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        )
 
         # Add handlers to logger
         self.logger.addHandler(info_handler)
@@ -44,11 +48,18 @@ class Logger:
         self.logger.error(message)
 
     def log_request(self, client_host, client_ip, user_agent, host, method, url):
-        self.info(f"Request: {method} {url}, Client Host: {client_host}, Client IP: {client_ip}, User-Agent: {user_agent}, Host: {host}")
-
-    def log_response(self, client_host, client_ip, user_agent, host, method, url, status_code):
         self.info(
-            f"Response: {status_code} {method} {url}, Client Host: {client_host}, Client IP: {client_ip}, User-Agent: {user_agent}, Host: {host}")
+            f"Request: {method} {url}, Client Host: {client_host}, Client IP: {client_ip}, User-Agent: {user_agent}, Host: {host}"
+        )
+
+    def log_response(
+        self, client_host, client_ip, user_agent, host, method, url, status_code
+    ):
+        self.info(
+            f"Response: {status_code} {method} {url}, Client Host: {client_host}, Client IP: {client_ip}, User-Agent: {user_agent}, Host: {host}"
+        )
 
     def log_exception(self, client_host, client_ip, user_agent, host, exception):
-        self.error(f"Exception: {str(exception)}, Client Host: {client_host}, Client IP: {client_ip}, User-Agent: {user_agent}, Host: {host}")
+        self.error(
+            f"Exception: {str(exception)}, Client Host: {client_host}, Client IP: {client_ip}, User-Agent: {user_agent}, Host: {host}"
+        )
