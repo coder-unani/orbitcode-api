312c2df8cf9543dd70547c847672af6b82d8c382
UNANI <coder.unani@gmail.com>
2024-06-27 16:43:16 +0900

    [fix] my review 관련 오류 수정

--- app/routes/v1/videos.py
+++ app/routes/v1/videos.py
@@ -342,18 +342,22 @@ async def read_video_myinfo(
             )
         my_is_like = await queryset.read_video_my_is_like(db, video_id, auth_user["id"])
         my_review = await queryset.read_video_my_review(db, video_id, auth_user["id"])
-        print(my_review.title)
+        if my_review:
+            disp_review = {
+                "id": my_review.id,
+                "title": my_review.title,
+                "content": my_review.content,
+            }
+        else:
+            disp_review = {}
+
         my_review_like = await queryset.read_video_my_review_like(
             db, video_id, auth_user["id"]
         )
         my_rating = await queryset.read_video_my_rating(db, video_id, auth_user["id"])
         myinfo = {
             "is_like": my_is_like,
-            "review": {
-                "id": my_review.id,
-                "title": my_review.title,
-                "content": my_review.content,
-            },
+            "review": disp_review,
             "review_like": my_review_like,
             "rating": my_rating,
         }
